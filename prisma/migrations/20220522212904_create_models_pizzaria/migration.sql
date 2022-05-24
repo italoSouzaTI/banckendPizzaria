3a7e8c0fb887ddbeec8cf931285fa3757bf73ca7
Italo Araujo <italoasouzat.i@gmail.com>
2022-05-24 08:47:09 -0300

    Ajustando campo

--- prisma/migrations/20220522212904_create_models_pizzaria/migration.sql
+++ prisma/migrations/20220522212904_create_models_pizzaria/migration.sql
@@ -29,7 +29,7 @@ CREATE TABLE "orders" (
     "id" TEXT NOT NULL,
     "table" INTEGER NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT false,
-    "draft" BOOLEAN NOT NULL DEFAULT false,
+    "draft" BOOLEAN NOT NULL DEFAULT true,
     "name" TEXT,
     "createde_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "update_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
